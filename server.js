e30777be6951750f217a7a933aa719e165c91b03
cespedes.elias97@gmail.com <elias.cespedes@rcpo.com.ar>
2025-07-16 17:37:36 -0300

    Template

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ import RouterNumeros from './router/numeros.js'
 import config from './config.js'
 
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json())//para decodificar los json recibidos
+app.use(express.urlencoded({extended: true}))//para el fomrulario en html
 
 app.use('/numeros', new RouterNumeros().start())
-app.use(express.static('public')) 
+app.use(express.static('public')) //poner publica la carpeta
 
 const PORT = config.PORT
 const server = app.listen(PORT, () => console.log(`Servidor ApiRestFul escuchando en http://localhost:${PORT}`))
